5391c8f1d65b33324f6affc3e9761cf299119a47
ruth <Vous@exemple.com>
2020-09-22 12:44:51 +0100

    initialisation du projet

---
+++ front-end/front-office/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <div className="container">
+        Bonjour
+       
+      </div>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ front-end/front-office/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+const Navbars = () => {
+  
+​
+  return (
+   <div>
+
+    <Navbar expand="lg" variant="light" bg="light">
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Navbar>
+    
+   </div>
+  )
+}
+​
+export default Navbars
